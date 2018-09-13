f8d7690d6b1eb5ebab0668afa938ead90a853303
tapsuno1 <stephenle1412@gmail.com>
2018-09-13 14:29:41 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (error) => {
+    if(error){
+      console.log('Unable to append to server log');
+    }
+  });
+
+  next();
+});
+
+app.use( (req, res, next) => {
+  res.render('maintenance.hbs');
+})
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('screamit', (text) => {
+  return text.toUpperCase();
+})
+
+app.get('/', (req, res) => {
+  //res.send('<h1>Hello Express!!</h1>');
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    WelcomeMessage: 'Welcome to my website',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: ' Unable to handle requets.'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('server up in port 3000');
+});
